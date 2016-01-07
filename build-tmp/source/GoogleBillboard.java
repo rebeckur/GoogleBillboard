ead0fade9a4455a9534672dce16e5dce565a8d62
unknown <releung1@697-334-Bywater.sf.k12.internal>
2016-01-07 10:30:34 -0900

    Finished

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class GoogleBillboard extends PApplet {
+
 public final static String e = "2718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
@@ -20,6 +36,11 @@ public void draw()
 }  
 public boolean isPrime(double dNum)  
 {   
+    //to be finished later
+    /*if (dNum < 2)
+    {
+    	return false;
+    }*/
     for (double i = 2; i <= Math.sqrt(dNum); i++)
     {
     	if (dNum % i == 0)
@@ -29,3 +50,12 @@ public boolean isPrime(double dNum)
     }
     return true;  
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
